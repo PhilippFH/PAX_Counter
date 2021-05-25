e8187d5846e02124000fff2eae704f3b692689c8
PhilippFH <84771365+PhilippFH@users.noreply.github.com>
2021-05-25 11:07:49 +0200

    Update PAX_Counter.R

--- PAX_Counter.R
+++ PAX_Counter.R
@@ -4,7 +4,7 @@ library(broom)
 library(ggplot2)
 library(dplyr)
 library(shiny)
-library(plotly)
+library(plotly)s
 library(lattice)
 library(lubridate)
 
@@ -12,11 +12,11 @@ library(lubridate)
 NHSBoards <- readOGR(dsn ="C:/Users/pmoer/Desktop/Shapefile") 
 NHSBoards_tidy <- tidy(NHSBoards)
 NHSBoards$id <- row.names(NHSBoards)
-NHSBoards_tidy <- merge(NHSBoards_tidy,datatest,by.x = "FID",by.y = "ROOM_ID")
+datatest$Time <- as.POSIXct(paste0(as.character(datatest$DATE),substr(as.character(datatest$TIME),11,20)))
+NHSBoards_tidy <- merge(NHSBoards_tidy,datatest,by.x = "id",by.y = "ROOM_ID")
 NHSBoards_tidy$Flooring_Time_Hour <- floor_date(NHSBoards_tidy$Time,"hour")
-NHSBoards_tidy_tmp <- NHSBoards_tidy %>% select(long,lat,NUMBER,FID,Flooring_Time_Hour,group)#%>% group_by(long,lat,FID,Flooring_Time_Hour,group) %>% summarise(Persons = NUMBER)
-
-ggplot(NHSBoards_tidy_tmp, aes(x = long, y = lat,group = group,fill = Persons)) +
+NHSBoards_tidy_tmp <- NHSBoards_tidy %>% select(long,lat,NUMBER,id,Flooring_Time_Hour,group)#%>% group_by(long,lat,FID,Flooring_Time_Hour,group) %>% summarise(Persons = NUMBER)
+ggplot(NHSBoards_tidy_tmp, aes(x = long, y = lat,group = group,fill = NUMBER)) +
   geom_polygon(color = "black", size = 0.1) +
   coord_equal() +
   scale_fill_gradient2(low = "white", mid = "yellow", high = "red", # colors
@@ -25,10 +25,14 @@ ggplot(NHSBoards_tidy_tmp, aes(x = long, y = lat,group = group,fill = Persons)) 
   labs(title = "FH Villach EG ein Raum") +
   theme(plot.title = element_text(margin = margin(t = 0, b = 0)))
 
+
+
+
+
 #Boxplot
 ggplot(datatest, aes(y = NUMBER )) + geom_boxplot(col = "black")  + labs(title = "Besucheranzahl")
 
 #For the line Diagram
 datatest$Time <- as.POSIXct(paste0(as.character(datatest$DATE),substr(as.character(datatest$TIME),11,20)))
-p<-ggplot(datatest, aes(x=Time,y=NUMBER)) + geom_line()+ labs(title = "Anzahl der Besucher über die Zeit")
+p<-ggplot(datatest, aes(x=Time,y=NUMBER)) + geom_line()+ labs(title = "Anzahl der Besucher Ã¼ber die Zeit")
 p
